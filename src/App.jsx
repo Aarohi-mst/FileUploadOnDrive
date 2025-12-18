62ebb030a7b4f4f53aa8d1861203a6bdde589f21
Aarohi <aarohi.verma@metasquaretech.com>
2025-12-18 18:27:51 +0530

    Clean initial commit (no uploads, no secrets)

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function App() {
+  const [file, setFile] = useState(null);
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+  const uploadFile = async () => {
+    if (!file) {
+      alert("Please select a file");
+      return;
+    }
+    const formData = new FormData();
+    formData.append("file", file);
+    try {
+      const res = await fetch("http://localhost:3000/upload", {
+        method: "POST",
+        body: formData,
+      });
+      const data = await res.json();
+      alert("File uploaded successfully");
+      console.log(data);
+    } catch (err) {
+      console.error("Error uploading file:", err);
+      alert("Error uploading file");
+    }
+  };
+
+  return (
+    <div>
+      <p>
+        <label htmlFor="fileUpload">Upload File:</label>
+        <br /> <br />
+        <input
+          type="file"
+          id="fileUpload"
+          placeholder="Upload your file"
+          onChange={(e) => handleFileChange(e)}
+        />
+        <br />
+      </p>
+
+      <button type="submit" onClick={uploadFile}>
+        Submit
+      </button>
+    </div>
+  );
+}
+
+export default App;
